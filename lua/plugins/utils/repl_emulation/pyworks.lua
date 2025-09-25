d1a3b00f6e101acfa05212b5c3bc74459e3f669a
Carl Aquino <c4rlaquino@gmail.com>
2025-09-25 00:11:25 -0600

    Reformatting, changing repl related stuff as their own folder

--- lua/plugins/utils/repl_emulation/pyworks.lua
+++ lua/plugins/utils/repl_emulation/pyworks.lua
@@ -12,15 +12,27 @@ return {
 			},
 			"3rd/image.nvim", -- Required: Image display
 		},
+		ft = { "python", "jupyter" },
+		event = { "BufReadPre *.py", "BufReadPre *.ipynb" },
 		config = function()
 			require("pyworks").setup({
 				-- Pyworks auto-configures everything with proven settings!
 				-- Just specify any preferences:
 				python = {
 					use_uv = true, -- Use uv for faster package installation
+					preferred_venv_name = ".venv",
+					auto_install_essentials = true,
+					essentials = { "pynvim", "ipykernel", "jupyter_client", "jupytext" },
 				},
-				image_backend = "kitty", -- or "ueberzug" for other terminals
+				image_backend = vim.fn.has("gui_running") == 1 and "kitty" and "ueberzug", -- or "ueberzug" for other terminals
 
+				on_notebook_open = function(bufnr)
+					vim.defer_fn(function()
+						vim.api.nvim_buf_call(bufnr, function()
+							vim.cmd("LspRestart")
+						end)
+					end, 500)
+				end,
 				-- Optional: Skip auto-configuration of specific dependencies
 				-- skip_molten = false,
 				-- skip_jupytext = false,
