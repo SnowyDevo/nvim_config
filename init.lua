ecbec94d79486973bb3523764352e03f384ae495
Carl Aquino <c4rlaquino@gmail.com>
2025-09-25 02:33:48 -0600

    fuck im getting close

--- init.lua
+++ init.lua
@@ -2,5 +2,3 @@ require("config.lazy")
 require("core.options")
 require("core.keymaps")
 vim.lsp.enable("pyright")
-vim.g.molten_auto_open_output = true
-vim.g.molten_default_kernel = "python3"
--- lua/plugins/utils/repl_emulation/pyworks.lua
+++ lua/plugins/utils/repl_emulation/pyworks.lua
@@ -1,17 +1,11 @@
 return {
 	{
 		"jeryldev/pyworks.nvim",
+		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
 			{
 				"GCBallesteros/jupytext.nvim",
-				config = function() -- IMPORTANT: This ensures jupytext.setup() is called!
-					require("jupytext").setup({
-						style = "percent",
-						output_extension = "auto", -- Default extension. Don't change unless you know what you are doing
-						force_ft = nil, -- Default filetype. Don't change unless you know what you are doing
-						custom_language_formatting = {},
-					})
-				end,
+				config = true,
 			},
 			{
 				"benlubas/molten-nvim", -- Required: Code execution
@@ -19,11 +13,8 @@ return {
 			},
 			"3rd/image.nvim", -- Required: Image display
 		},
-		ft = { "python", "jupyter" },
-		event = { "BufReadPre *.py", "BufReadPre *.ipynb" },
 		config = function()
 			require("pyworks").setup({
-				-- Pyworks auto-configures everything with proven settings!
 				-- Just specify any preferences:
 				python = {
 					use_uv = true, -- Use uv for faster package installation
@@ -31,17 +22,8 @@ return {
 					auto_install_essentials = true,
 					essentials = { "pynvim", "ipykernel", "jupyter_client", "jupytext" },
 				},
-
 				--Conditional for ueberzug or kitty
 				image_backend = "kitty", -- or "ueberzug" for other terminals
-
-				on_notebook_open = function(bufnr)
-					vim.defer_fn(function()
-						vim.api.nvim_buf_call(bufnr, function()
-							vim.cmd("LspRestart")
-						end)
-					end, 500)
-				end,
 				-- Optional: Skip auto-configuration of specific dependencies
 				-- skip_molten = false,
 				-- skip_jupytext = false,
@@ -49,7 +31,5 @@ return {
 				-- skip_keymaps = false,
 			})
 		end,
-		lazy = false, -- Load immediately for file detection
-		priority = 100, -- Load early
 	},
 }
