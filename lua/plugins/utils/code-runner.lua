9529c8e7a5323399995f3c32d89e7fbcf3563e9a
Carl Aquino <c4rlaquino@gmail.com>
2025-10-03 17:17:19 -0600

    test

--- lua/plugins/utils/code-runner.lua
+++ lua/plugins/utils/code-runner.lua
@@ -1,20 +1,23 @@
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+
 return {
 	"CRAG666/code_runner.nvim",
-	config = true,
-	filetype = {
-		java = {
-			"cd $dir &&",
-			"javac $fileName &&",
-			"java $fileNameWithoutExt",
-		},
-		python = "python3 -u",
-		typescript = "deno run",
-		rust = {
-			"cd $dir &&",
-			"rustc $fileName &&",
-			"$dir/$fileNameWithoutExt",
-		},
-	},
+	config = function()
+		require("code_runner").setup({
+			filetype = {
+				java = {
+					[[java -cp "out/production/]] .. project_name .. [[" Main]],
+				},
+				python = "python3 -u",
+				typescript = "deno run",
+				rust = {
+					"cd $dir &&",
+					"rustc $fileName &&",
+					"$dir/$fileNameWithoutExt",
+				},
+			},
+		})
+	end,
 	vim.keymap.set("n", "<leader>rr", ":RunCode<CR>", { noremap = true, silent = false }),
 	vim.keymap.set("n", "<leader>rf", ":RunFile<CR>", { noremap = true, silent = false }),
 	vim.keymap.set("n", "<leader>rft", ":RunFile tab<CR>", { noremap = true, silent = false }),
--- lua/plugins/utils/nvim-dap.lua
+++ lua/plugins/utils/nvim-dap.lua
@@ -1,1 +1,11 @@
-
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui", -- optional UI
+		"theHamsta/nvim-dap-virtual-text", -- inline variable display
+	},
+	config = function()
+		require("dapui").setup()
+		require("nvim-dap-virtual-text").setup()
+	end,
+}
