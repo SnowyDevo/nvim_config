5f69ff85c204c0e9b6781c8c319b6647392857ed
Carl Aquino <c4rlaquino@gmail.com>
2025-10-08 01:24:16 -0600

    fix

--- lua/plugins/utils/code-runner.lua
+++ lua/plugins/utils/code-runner.lua
@@ -1,20 +1,43 @@
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+
+local function find_main_class()
+	local output_dir = "out/production/" .. project_name
+
+	local main_class_files = vim.fn.globpath(output_dir, "**/Main.class", false, true)
+
+	if #main_class_files > 0 then
+		local main_class_path = main_class_files[1]
+
+		local relative_path = main_class_path:gsub("^" .. vim.pesc(output_dir) .. "/", "")
+
+		local fully_qualified = relative_path:gsub("/", "."):gsub("%.class$", "")
+
+		print("Found main class: " .. fully_qualified)
+
+		return fully_qualified
+	end
+	return "Main" --fallback
+end
+
 return {
 	"CRAG666/code_runner.nvim",
-	config = true,
-	filetype = {
-		java = {
-			"cd $dir &&",
-			"javac $fileName &&",
-			"java $fileNameWithoutExt",
-		},
-		python = "python3 -u",
-		typescript = "deno run",
-		rust = {
-			"cd $dir &&",
-			"rustc $fileName &&",
-			"$dir/$fileNameWithoutExt",
-		},
-	},
+	config = function()
+		require("code_runner").setup({
+			filetype = {
+				java = {
+					[[javac -d "out/production/$(basename $PWD)" $(find src -name "*.java" -not -path "src/test/*") &&]],
+					[[java -cp "out/production/]] .. project_name .. [[" ]] .. find_main_class(),
+				},
+				python = "python3 -u",
+				typescript = "deno run",
+				rust = {
+					"cd $dir &&",
+					"rustc $fileName &&",
+					"$dir/$fileNameWithoutExt",
+				},
+			},
+		})
+	end,
 	vim.keymap.set("n", "<leader>rr", ":RunCode<CR>", { noremap = true, silent = false }),
 	vim.keymap.set("n", "<leader>rf", ":RunFile<CR>", { noremap = true, silent = false }),
 	vim.keymap.set("n", "<leader>rft", ":RunFile tab<CR>", { noremap = true, silent = false }),
